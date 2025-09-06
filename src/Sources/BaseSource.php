0c119ada0d5ff733b5e24fdebe0fe9bc351032f1
Chuck Greenman <charlesgreenman@gmail.com>
2025-09-06 16:48:07 -0400

    New approach.

---
+++ src/Sources/BaseSource.php
@@ -0,0 +1,24 @@
+<?php
+
+interface BaseSource
+{
+    /**
+     * Returns the number of tables in a sources.
+     *
+     * @return int
+     */
+    public function numberOfTables(): int;
+
+    /**
+     * Returns a list of table names as an array of strings from current soruce.
+     *
+     * @return array.
+     */
+    public function listOfTables(): array;
+
+    /**
+     * Returns an array of Columns
+     * @return array
+     */
+    public function listColumns(stirng $tableName): array;
+}
